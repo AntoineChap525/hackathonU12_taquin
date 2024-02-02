35e65fe9142f968096061b17f348cc133e87c998
Deddivax <david.abulius@gmail.com>
2024-02-02 11:26:15 +0100

    implémentation recherche de voisins

--- convert.py
+++ convert.py
@@ -9,8 +9,7 @@ def from_tab_to_str(tab):
 
 def from_str_to_tab(string):
     n = int(sqrt(len(string)))
-    tab = [[None]*n]*n
-    for i in range(n):
-        for j in range(n):
-            tab[i][j] = string[n*i+j]
+    tab = [[None]*n for _ in range(n)]
+    for j in range(n):
+        tab[i][j] = string[n*i + j]
     return tab
