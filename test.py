a305a81ea5c1302f81ffa41c24b68fc530467d4f
Samy Smail <smail.samy2003@gmail.com>
2024-02-02 12:23:14 +0100

    Fonction concaténation

---
+++ test.py
@@ -0,0 +1,24 @@
+#une fonction qui concatène les éléments d'une liste de taille 3*3 et les transforme en string, et renvoie aussi le couple qui donne les coordonnées du 0
+
+def concatene(liste):
+    res = ""
+    for i in range(3):
+        for j in range(3):
+            if liste[i][j] == 0:
+                couple = (i +1 , j + 1)
+                res += str(liste[i][j])
+            else:
+                res += str(liste[i][j])
+    return res, couple
+
+print(concatene([[1, 2, 3], [4, 0, 5], [6, 7, 8]]))
+
+def concatene9(liste):
+    chaine = ""
+    for i in range(len(liste)):
+        chaine += str(liste[i])
+    return chaine
+
+print(concatene9([1, 2, 3, 0, 4, 5, 6]))
+
+
